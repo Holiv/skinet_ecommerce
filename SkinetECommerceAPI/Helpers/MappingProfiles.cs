c7f726d84061a75a040dd38aec10d271b111ebb5
holiv <mrheltonso@gmail.com>
2023-07-13 23:26:57 -0300

    setting resolver for mapping url correctly and adding the API static files service

--- SkinetECommerceAPI/Helpers/MappingProfiles.cs
+++ SkinetECommerceAPI/Helpers/MappingProfiles.cs
@@ -12,7 +12,8 @@ namespace SkinetECommerceAPI.Helpers
 		{
 			CreateMap<Products, ProductToReturnDto>()
 				.ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-				.ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+				.ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+				.ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
 
         }
 	}
